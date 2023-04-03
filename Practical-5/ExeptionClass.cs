42ed9d6aab92c52a5a5ba47e027560356fdaf753
Gaurav Mori <gkmori444@gmail.com>
2023-04-03 15:33:31 +0530

    Practical-5
    Inside Practical-5 Feature Branch

---
+++ Practical-5/ExeptionClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_5
+{
+      class ExeptionClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] integerArray = new int[] { 211, 321, 431,541, 651 };
+            Console.WriteLine("Index Value");
+                for(int i=0;i<integerArray.Length;i++)
+            {
+                Console.WriteLine("  "+i +"   "+integerArray[i]);
+            }
+
+            try
+            {
+                Console.WriteLine("\nInside Try and Catch");
+                for (int i = 0; i < 6; i++)
+                {
+                    Console.WriteLine("  " + i + "   " + integerArray[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+               
+            }
+            finally { Console.WriteLine("\n\nInside Finally Block ."); }
+        
+        }
+    }
+}
